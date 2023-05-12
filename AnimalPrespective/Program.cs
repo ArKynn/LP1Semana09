44977bffc5a378beed843f06c63299ed9d908c80
David Mendes <david.f.mendes@proton.me>
2023-05-12 18:04:21 +0100

    Added Main

--- AnimalPrespective/Program.cs
+++ AnimalPrespective/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Polimorfismo;
 
-Console.WriteLine("Hello, World!");
+namespace AnimalPrespective;
+
+public class Program
+{
+    public static void Main()
+    {
+        Animal[] animals = new Animal[10];
+        Random random = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            animals[i] = random.Next(4) switch
+            {
+                0 => new Bat(),
+                1 => new Bee(),
+                2 => new Cat(),
+                3 => new Dog(),
+                _ => throw new ArgumentOutOfRangeException("Unknown animal!")
+            };
+        }
+
+
+        foreach (Animal animal in animals)
+        {
+            Console.WriteLine($"{animal.Sound()}");
+            if (animal is IMammal)
+            {
+                Console.WriteLine($"Nº of Nipples: {(animal as IMammal)!.NumberOfNipples}"); 
+            }
+
+            if (animal is ICanFly)
+            {
+                Console.WriteLine($"Nº of Wings: {(animal as ICanFly)!.NumberOfWings}");
+            }
+        }
+    }
+}
